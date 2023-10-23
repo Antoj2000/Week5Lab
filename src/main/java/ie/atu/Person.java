5b8c1b4d67fd49ad038cbb7355ee221e4463f45e
G00385306@atu.ie <g385306@atu.ie>
2023-10-23 10:38:25 +0100

    Added a user input function

--- src/main/java/ie/atu/Person.java
+++ src/main/java/ie/atu/Person.java
@@ -1,5 +1,7 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Person {
     public Person() {
         this.firstName = "";
@@ -40,7 +42,22 @@ public class Person {
     private String lastName;
     private int age;
 
+    public void getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first name: ");
+        this.firstName = scanner.nextLine();
+
+        System.out.print("Enter last name: ");
+        this.lastName = scanner.nextLine();
+
+        System.out.print("Enter age: ");
+        this.age = scanner.nextInt();
+    }
+
     public void displayInfo(){
         System.out.println("Name: " + firstName + " " + lastName + ", Age: " + age);
     }
+
+
 }
